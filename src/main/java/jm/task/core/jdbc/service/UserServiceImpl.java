3e758ddd0aae6411dba071649f8afdb3aebb9784
Aleksandr-K88 <bon290688@mail.ru>
2023-11-21 00:18:40 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoJDBCImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
+
     public void createUsersTable() {
         userDao.createUsersTable();
     }
@@ -25,13 +27,13 @@ public class UserServiceImpl implements UserService {
 
     public List<User> getAllUsers() {
         List<User> users = userDao.getAllUsers();
-        for (User user: users){
+        for (User user : users) {
             System.out.println(user);
         }
         return users;
     }
 
     public void cleanUsersTable() {
-       userDao.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
